fc2285e2386ca99603eb61d435dce6416ed114aa
charlesduffour1 <143336723+charlesduffour1@users.noreply.github.com>
2023-08-27 15:06:10 -0500

    Update main.tf

--- Terraform/region/main.tf
+++ Terraform/region/main.tf
@@ -99,6 +99,7 @@ resource "azurerm_lb" "lb" {
 
 # Define virtual machine
 resource "azurerm_virtual_machine" "vm" {
+  count                 = 2
   name                  = "${var.region}-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
