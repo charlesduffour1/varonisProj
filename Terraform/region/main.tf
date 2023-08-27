4cca036dfbb96321779428c02fc270e7d917d27a
charlesduffour1 <143336723+charlesduffour1@users.noreply.github.com>
2023-08-27 14:56:17 -0500

    Update main.tf for firewall

--- Terraform/region/main.tf
+++ Terraform/region/main.tf
@@ -68,7 +68,7 @@ resource "azurerm_network_security_group" "nsg" {
 
 
 resource "azurerm_firewall" "firewall" {
-  name                = "${var.region}-firewall"
+  name                = "byfirewall"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
